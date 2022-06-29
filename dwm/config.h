6c286895a4e90b8386874fe4e8565ea8749b2959
Thamognya Kodi <contact@thamognya.com>
2022-06-29 09:12:55 +0700

    using kitty now

--- dwm/config.h
+++ dwm/config.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-p", "Apps", "-l", "20", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *screenshotcmd[]  = { "flameshot", "gui", NULL };
 static const char *increasevol[]  = { "pactl", "set-sink-volume", "0", "+5%", NULL };
 static const char *decreasevol[]  = { "pactl", "set-sink-volume", "0", "-5%", NULL };
